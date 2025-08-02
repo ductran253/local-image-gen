c056380c2a83398625eba76591584c3aaf343bdc
Duc Tran <DucMinhKhoi.Tran@student.uts.edu.au>
2025-08-02 21:24:26 +1000

    Add Clean up feature at click

--- backend/model.py
+++ backend/model.py
@@ -3,6 +3,7 @@ import torch
 import os
 import sys
 from diffusers import StableDiffusionXLPipeline
+from diffusers.hooks import apply_group_offloading
 
 torch.backends.cuda.matmul.allow_tf32 = True
 
@@ -14,24 +15,38 @@ else:
 
 model_path = os.path.abspath(os.path.join(base_path, "../model"))
 
-max_memory = {0:"8GB", "cpu":"8GB"}
+max_memory = {"cuda":"7GB","cpu":"8GB"}
 #Load Model
 
 #Lazy Loading approach
-pipeline = AutoPipelineForText2Image.from_pretrained(
+pipeline = StableDiffusionXLPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",
-    torch_dtype=torch.bfloat16,
+    torch_dtype=torch.float16,
     variant="fp16",
     use_safetensors=True,
     cache_dir=model_path,
-    low_cpu_mem_usage=False
-).to("cuda")
+    low_cpu_mem_usage=True,
+    max_memory=max_memory
+)
 
 pipeline.enable_model_cpu_offload()
+
 pipeline.enable_vae_tiling()
+
 pipeline.enable_attention_slicing()
 
 def generate_image(prompt, output_path="output.png"):
-    image = pipeline(prompt, num_inference_steps=30).images[0]
+    import torch
+    import gc
+
+    # Run inference
+    result = pipeline(prompt, num_inference_steps=30)
+    image = result.images[0]
     image.save(output_path)
+
+    # Explicitly delete unnecessary stuff
+    del result
+    torch.cuda.empty_cache()
+    gc.collect()
+
     return output_path
